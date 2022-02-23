5605bd9ff24a20ab70384ee78bf64ad949764c3a
Stephen Hartke <hartke@gmail.com>
2022-02-23 12:24:26 -0700

    Adding lower bounds for clique_square.sh

--- scripts/clique_square.sh
+++ scripts/clique_square.sh
@@ -2,16 +2,22 @@
 
 # bash script to read multiple graph6 strings in on stdin.
 # Each graph is squared, converted to DIMACS format, and then fed into exactcolors's stable program.
-# The output is parsed, and if the optimal clique number is less than an upper bound, the original graph6 string is written to stdout.
+# The output is parsed, and if the optimal clique number is less than an upper bound (or at least a lower bound), the original graph6 string is written to stdout.
 
 if [ "$1" == "" ]
 then
   upperboundlimit=7
 else
   upperboundlimit="$1"
+  if [ "$2" == "" ]
+  then
+    lowerboundlimit=1
+  else
+    lowerboundlimit="$2"
+  fi
 fi
 
-n=0
+count=0
 while read -r line
 do
   tmpfile="$(mktemp /tmp/color_g6.XXXXXXXXXX)"
@@ -20,12 +26,18 @@ do
   if [ "$cliquesize" -lt "$upperboundlimit" ]
   then
     echo $line
+  else
+    if [ "$cliquesize" -ge "$lowerboundlimit" ]
+    then
+      echo $line
+    fi
   fi
   rm -f "$tmpfile"
   
-  n=$(( n+1 ))
-  if [ "$(( n % 1000 ))" -eq 0 ]
+  count=$(( count+1 ))
+  if [ "$(( count % 1000 ))" -eq 0 ]
   then
-     echo "Testing n=$n" 1>&2
-  fi 
+     # output count to stderr
+     echo "Testing count=${count}" 1>&2
+  fi
 done
